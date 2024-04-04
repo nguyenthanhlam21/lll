2713aff3674d4eb422f752bd5413ea30dfa648b5
nguyenthanhlam21 <lamn47593@gmail.com>
2024-04-04 12:07:26 +0700

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={style.footer}>
+      <div>copyright by lamntph31955</div>
+    </div>
+  );
+};
+
+export default Footer;
